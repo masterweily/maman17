eeae954a33a16d3bc43dab39599dc71560ed28b9
masterweily <masterweily@gmail.com>
2013-07-27 16:41:12 +0300

    asdf

--- app.rb
+++ app.rb
@@ -1,13 +1,19 @@
 $: << 'lib'
 $: << '.'
 require 'yw_aima'
+require 'awesome_print'
 
 module App
   extend ActiveSupport::Autoload
 
   autoload :Data
+  Learner = YW_AIMA::Learning::Decision::Learner
 
   examples = Data.examples
+  tree = Learner.tree_learn(examples)
+
+  ap tree.to_hash
+  puts tree.to_hash.to_yaml
 
 end
 
--- lib/yw_aima/learning/decision/tree.rb
+++ lib/yw_aima/learning/decision/tree.rb
@@ -1,3 +1,4 @@
+require 'json'
 class YW_AIMA::Learning::Decision::Tree
 
   def initialize(value)
@@ -17,5 +18,33 @@ class YW_AIMA::Learning::Decision::Tree
   def leaf?
     @branches.empty?
   end
+
+  def to_hash
+    if leaf?
+      @value
+    else
+      attribute = @value
+      branches = {}
+      @branches.each do |k,v|
+        branches[k] = v.to_hash
+      end
+      return { attribute => branches }
+    end
+    #hash = { value: @value, branches: {}}
+    #@branches.each do |k,v|
+    #  hash[:branches][k] = v.to_hash
+    #end
+    #return hash
+
+  end
+
+  def to_s
+    to_hash.to_s
+  end
+
+  def to_json
+    to_hash.to_json
+  end
+
 
 end
